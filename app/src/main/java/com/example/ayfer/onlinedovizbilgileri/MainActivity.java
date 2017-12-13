5443effe492e6142adde81cb6eccaf2ca6768c6c
esmasahin <34026419+esmasahin@users.noreply.github.com>
2017-12-13 16:34:34 +0300

    http nesneleri için gradle güncellendi

--- app/src/main/java/com/example/ayfer/onlinedovizbilgileri/MainActivity.java
+++ app/src/main/java/com/example/ayfer/onlinedovizbilgileri/MainActivity.java
@@ -6,6 +6,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
     public static final String URL = "http://www.doviz.gen.tr/doviz_json.asp?version=1.0.4";
 
@@ -32,5 +39,8 @@ public class MainActivity extends AppCompatActivity {
         final float tlcon1=Float.parseFloat(tlcon.getText().toString());
         final float dolarcon1=Float.parseFloat(dolarcon.getText().toString());
         final float eurocon1=Float.parseFloat(eurocon.getText().toString());
+
+
     }
+
 }
