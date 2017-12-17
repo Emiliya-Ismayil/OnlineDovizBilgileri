220e3d4f89760bd2ab4dc6fccc4c68d95ce67875
esmasahin <34026419+esmasahin@users.noreply.github.com>
2017-12-17 17:36:38 +0300

    firebase servisleri eklendi.

---
+++ app/src/main/java/com/example/ayfer/onlinedovizbilgileri/MyFirebaseInstanceIDService.java
@@ -0,0 +1,24 @@
+package com.example.ayfer.onlinedovizbilgileri;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Esma on 17.12.2017.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        String token = FirebaseInstanceId.getInstance().getToken();
+        registerToken(token);
+        Log.d("TOKEN Verildi", token);
+    }
+
+    private void registerToken(String token){
+
+    }
+}
